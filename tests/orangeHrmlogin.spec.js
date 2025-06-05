e1848674c309c11f3581caa4af0bb66cf19930f7
Purushotham35 <purushothammdk2@gmail.com>
2025-06-05 14:57:51 +0530

    Initial commit

---
+++ tests/orangeHrmlogin.spec.js
@@ -0,0 +1,40 @@
+import{test , expect } from "@playwright/test"
+test("verify login with valid credentials",async({page})=>{
+await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+await page.locator("input[name='username']").fill("Admin")
+await page.locator("input[type='password']").fill("admin123")
+await page.locator("button[type='submit']").click()
+
+
+await expect(page).toHaveURL("https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index")
+
+})
+test("verify login with valid username and invalid password",async({page})=>{
+
+await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+await page.locator("input[name='username']").fill("Admin")
+await page.locator("input[type='password']").fill("admin12")
+await page.locator("button[type='submit']").click()
+await expect(page.locator("p.oxd-text.oxd-text--p.oxd-alert-content-text")).toBeVisible("Invalid credentials")
+
+
+
+})
+test("verify login with invalid username and valid password",async({page})=>{
+
+await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+await page.locator("input[name='username']").fill("Adminfjvh")
+await page.locator("input[type='password']").fill("admin123")
+await page.locator("button[type='submit']").click()
+await expect(page.locator("p.oxd-text.oxd-text--p.oxd-alert-content-text")).toBeVisible("Invalid credentials")
+
+})
+
+test("verify login with invalid username and invalid password",async({page})=>{
+
+await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+await page.locator("input[name='username']").fill("Admin123")
+await page.locator("input[type='password']").fill("adminccm12")
+await page.locator("button[type='submit']").click()
+await expect(page.locator("p.oxd-text.oxd-text--p.oxd-alert-content-text")).toBeVisible("Invalid credentials")
+})
---
+++ tests/swagerlogin.spec.js
@@ -0,0 +1,41 @@
+import { test, expect } from '@playwright/test';
+ test("verify login functionality with valid credentials",async({page})=>{
+await page.goto("https://www.saucedemo.com/v1/")
+await page.locator("input[data-test='username']").fill("standard_user")
+await page.locator("input[type='password']").fill("secret_sauce")
+await page.locator("input[type='submit']").click()
+
+await expect(page).toHaveURL("https://www.saucedemo.com/v1/inventory.html")
+
+
+ })
+
+ 
+test("verify login functionality with invalid username and valid password",async({page})=>{
+await page.goto("https://www.saucedemo.com/v1/")
+await page.locator("input[data-test='username']").fill("locked_out_user")
+await page.locator("input[type='password']").fill("secret_sauce")
+await page.locator("input[type='submit']").click()
+await expect(page.locator("h3[data-test='error']")).toContainText("Sorry, this user has been locked out")
+})
+
+
+test("verify login functionality with valid username and invalid password" ,async({page})=>{
+await page.goto("https://www.saucedemo.com/v1/")
+await page.locator("input[data-test='username']").fill("standard_user")
+await page.locator("input[type='password']").fill("abcdefg")
+await page.locator("input[type='submit']").click()
+await expect(page.locator("h3[data-test='error']")).toBeVisible("Epic sadface: Username and password do not match any user in this service")
+})
+
+ test("verify  both invalid username and password ", async({page})=>{
+
+
+await page.goto("https://www.saucedemo.com/v1/")
+await page.locator("input[data-test='username']").fill("performar")
+await page.locator("input[type='password']").fill("secrsauce")
+await page.locator("input[type='submit']").click()
+
+await expect(page.locator("h3[data-test='error']")).toBeVisible("Epic sadface: Username and password do not match any user in this service")
+
+ })
