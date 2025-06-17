58237f7d5543801249c54a4ef4bba00aeec0cc41
Purushotham35 <purushothammdk2@gmail.com>
2025-06-17 22:58:06 +0530

    assisgnment3

--- tests/orangeHrmlogin.spec.js
+++ tests/orangeHrmlogin.spec.js
@@ -5,8 +5,8 @@ import data from "../testdata/login.json"
 
 test("verify login with valid credentials",async({page})=>{
 await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-await page.locator("input[name='username']").fill(data.username)
-await page.locator("input[type='password']").fill(data.password)
+await page.locator("input[name='username']").fill(process.env.ORG_USERNAME)
+await page.locator("input[type='password']").fill(process.env.ORG_PASSWORD)
 await page.locator("button[type='submit']").click()
 
 
