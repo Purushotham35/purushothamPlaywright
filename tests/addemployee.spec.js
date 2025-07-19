8604d5ea7da894d0bfc7d5305bd0279bf395004c
Purushotham35 <purushothammdk2@gmail.com>
2025-07-19 18:06:48 +0530

    On branch US_1123-addJob Changes to be committed: 	modified:   testdata/add job title.json 	modified:   tests/addemployee.spec.js

--- tests/addemployee.spec.js
+++ tests/addemployee.spec.js
@@ -1,28 +1,21 @@
-import{test,expect} from '@playwright/test'
+import { test, expect } from "@playwright/test";
 
-import{ loginPage } from '../page object model/loginPage.po'
-import {addEmp} from '../page object model/dashboardPage.po'
-import data from '../testdata/login.json'
-  let page
-  let dB
-  let login
+import { loginPage } from "../page object model/loginPage.po";
+import { addEmp } from "../page object model/dashboardPage.po";
+import data from "../testdata/login.json";
+let page;
+let dB;
+let login;
 
-    test.beforeEach(async({browser})=>{
-        page=await browser.newPage()
-        dB= new addEmp(page)
-        
-        login=new loginPage(page)
-        await login.launchUrl()
-        await  login.loginWithCreds(data.username,data.password)
-       
+test.beforeEach(async ({ browser }) => {
+  page = await browser.newPage();
+  dB = new addEmp(page);
 
-    })
+  login = new loginPage(page);
+  await login.launchUrl();
+  await login.loginWithCreds(data.username, data.password);
+});
 
-    
-    
-
-test("add emp",async()=>{
-
-await dB.addEmp1("purushotham","md",'testdata/files/images cheeatah.jpeg')
-
-})
+test("add emp", async () => {
+  await dB.addEmp1("purushotham", "md", "testdata/files/images cheeatah.jpeg");
+});
